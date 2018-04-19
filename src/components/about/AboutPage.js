b68df202baf716c68e2e5252b37316d22bca8386
pd1009 <pdadhich.654@gmail.com>
2018-04-20 02:13:40 +0530

    Added routes in the app using react router
    Signed-off-by: pd1009 <pdadhich.654@gmail.com>

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>About</h1>
+				<p>This is a course about making apps with react redux es6</p>
+			</div>
+		);
+	}
+}
+
+export default AboutPage;
